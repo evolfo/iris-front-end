fd36a1b75cc4199d93743e86e89c58cb1250f750
Angelo Spampinato <spampinatodesign@gmail.com>
2019-12-02 12:47:37 -0500

    changing to soundcloud link

--- src/components/MusicStream.js
+++ src/components/MusicStream.js
@@ -3,8 +3,8 @@ import React from 'react'
 const MusicStream = props => {
   return (
     <section className="App-header">
-      <div className="container main">
-        <iframe src="https://open.spotify.com/embed/playlist/7gTHWr0Kbbfqav0mpWLaPb" width="600" height="880" frameborder="0" allowtransparency="true" allow="encrypted-media" title="spotify"></iframe>
+      <div className="container main music-stream">
+        <iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/934329787&color=%23201418&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
       </div>
     </section>
   )
