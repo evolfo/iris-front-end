da9d51cf24a8b7300d0ceeecbc69fd55bc64475f
Angelo Spampinato <spampinatodesign@gmail.com>
2020-01-05 17:29:09 -0500

    updating the vinyl/vip offers and change vip-bundle image to include the three cd

--- src/containers/VipOffer.js
+++ src/containers/VipOffer.js
@@ -25,7 +25,7 @@ class VipOffer extends Component {
 
  	  // purchaseObj is being sent to my backend
       const purchaseObj = {
-        amount: 75,
+        amount: 65,
         bundleName: 'VIP bundle',
         userId: this.props.mainReducer.user.user.id
 	  }
@@ -74,20 +74,20 @@ class VipOffer extends Component {
 				    </div>
 				    <div className="col-6">
 				      <h3>Yes, I Want The VIP Merch Bundle</h3>
-				      <h4>(Save $75)</h4>
+				      <h4>(Save $65)</h4>
 				      <p>Thank you so much for supporting my music! You're the reason that I get to keep making music, and I want to give you 50% off my latest VIP merch bundle to show my appreciation.</p>
 				      <p>This is also my way of sharing a different part of me with you. All of the items in the VIP bundle are dear to my heart, and are extremely unique. From my very own Iris Lune hot sauce, to a one-of-a-kind vinyl recording and a surprise Lunebag, this collection is both practical and inspiring.</p>
 				      <p>Here's what I'll send straight to your door...</p>
 				      <ul>
 				        <li><strong>Eclipse 5 oz Hot Sauce</strong> ($25 value)</li>
-				        <li><strong>Audio Polaroid 7" Record*</strong> ($50 value)</li>
+				        <li><strong>Limited Edition Three CD*</strong> ($30 value)</li>
 				        <li><strong>Tote Bag </strong>($40 value)</li>
 				        <li><strong>Eclipse 1 oz Hot Sauce</strong> ($8 value)</li>
 				        <li><strong>Lunebag** </strong> ($25 value)</li>
 				        <li><strong>"Stories in Glass" Sticker </strong> ($2 value)</li>
 				      </ul>
-				      <button onClick={this.props.vipModalOpen}>Yes! I Want the VIP Upgrade ($75)</button>
-				      <p>*Audio Polaroid 7" Record: a one-of-a-kind performance of me and my band playing one of our songs live. Each recording is done in a single take directly to vinyl in one of Brooklyn's top recording studios.</p>
+				      <button onClick={this.props.vipModalOpen}>Yes! I Want the VIP Upgrade ($65)</button>
+				      <p>*Limited Edition "Three" CD: Limited printing of all Iris Lune songs to date. Only 200 copies were made and once they’re gone, they’re gone! Grab yours now.</p>
 				      <p>**Lunebag: A collection of small items that inspire me. No two bags are the same; every bag is unique!</p>
 				    </div>
 				</div>
